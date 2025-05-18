b94ec798db9357c46665c0b14eb8926eb8abe2bb
Disha Narang <dishanarang2006@gmail.com>
2025-05-18 22:43:32 +0530

    all setup configured

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,15 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const registerUser=asyncHandler(async (req,res)=>{
+    res.status(200).json({
+        message:"chai aur code"
+    })
+})
+// const loginUser=asyncHandler(async (req,res)=>{
+//     res.status(200).json({
+//         message:"ok"
+//     })
+// })
+
+export {registerUser}
